5e62e0b4cd4136a375d49cc14fca84aee8fee312
wangqing <408564126@qq.com>
2016-09-06 09:17:45 +0800

    jpushPersonController

---
+++ src/com/yz/controller/JPushPersonController.java
@@ -0,0 +1,24 @@
+package com.yz.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.yz.po.Jpushperson;
+import com.yz.service.JpushpersonService;
+
+@Controller
+public class JPushPersonController {
+	
+	@Autowired
+	private JpushpersonService jpushpersonService;
+	
+	@RequestMapping("/jpushpersons")
+	public List<Jpushperson> findJPushPersonList() throws Exception {
+		
+		return jpushpersonService.findJPushPersonList();
+	}
+
+}
